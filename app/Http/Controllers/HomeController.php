c99adeac626d5c3f7eb650529337c5a9fe800568
Mikko Paulo Guevarra <emphigie10@gmail.com>
2021-01-19 23:38:23 +0800

    error

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Dress;
 
 class HomeController extends Controller
 {
-    //
+    public function index() {
+        $all_dress = Dress::all();
+        $data = [
+            'testo' => 'ciaooooo',
+            'dress' => $all_dress
+        ];
+        return view('home', $data);
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,6 @@
 
 @section('content')
 <h1>home page</h1>
+<p>{{$testo}}</p>
+<p>{{$dress->name}}</p>
 @endsection
